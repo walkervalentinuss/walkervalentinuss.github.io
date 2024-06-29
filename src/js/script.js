eb683094eed5ce8366020f8c0860910a5e2223c4
walkervalentinuss@gmail.com <walkervalentinuss@gmail.com>
2024-06-29 20:13:29 +0700

    memperbaiki isi folder

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
-// Navbar Fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+// // Navbar Fixed
+// window.onscroll = function() {
+//     const header = document.querySelector('header');
+//     const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-};
+//     if(window.pageYOffset > fixedNav){
+//         header.classList.add('navbar-fixed');
+//     } else {
+//         header.classList.remove('navbar-fixed');
+//     }
+// };
 
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
